ee194279f8708baaefc56cc070feeb2f7baee5e0
maksimusii <maksimus@rtural.ru>
2021-12-11 19:49:51 +0500

    Delete console.log

--- src/calc/calc.controller.ts
+++ src/calc/calc.controller.ts
@@ -20,7 +20,6 @@ export class CalcController {
     @Query() query?: Values,
     @Body() values?: Values,
   ): number | string {
-    console.log(Body);
     const val1 = query.val1 | values.val1;
     const val2 = query.val2 | values.val2;
     return this.CalcService.calc(headers['type-operation'], val1, val2);
